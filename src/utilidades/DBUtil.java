6ce336509177b3c026651ca6c780d91541bd229c
sro2005 <santiagor.o06105@gmail.com>
2024-05-28 15:53:00 -0500

    Actualizar archivo

--- src/utilidades/DBUtil.java
+++ src/utilidades/DBUtil.java
@@ -5,13 +5,11 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBUtil {
-    // Configuración de la conexión a la base de datos
-    private static final String URL = "http://localhost/phpmyadmin/index.php?route=/database/structure&db=projecthomeappliancessro";
-    private static final String USUARIO = "root";
-    private static final String CONTRASENA = "";
+    private static final String URL = "jdbc:mysql://localhost:3306/home_appliances";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
 
-    // Método para obtener una conexión a la base de datos
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USUARIO, CONTRASENA);
+        return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
