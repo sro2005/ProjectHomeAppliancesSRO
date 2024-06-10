fb5f4f13b23ff2404763cc7cabeb63299d160148
sro2005 <santiagor.o06105@gmail.com>
2024-06-09 19:24:02 -0500

    Actualizar cambios

---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // Este método de prueba verifica que la aplicación se cargue correctamente.
+    }
+
+}
